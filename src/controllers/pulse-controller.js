3a690853f31464eab83d1c0c615fe36d5cf74806
Momchil Marinov <mmarinov@spbtv.com>
2016-03-09 21:41:55 +0100

    optimize response json

--- src/controllers/pulse-controller.js
+++ src/controllers/pulse-controller.js
@@ -25,7 +25,14 @@ module.exports.getByVideo = (req, res, next)=> {
     if (!data) {
       return res.status(404).send();
     }
-    res.json(parseBeats(data));
+    let beats = [];
+    data.forEach(item=> {
+      let beat = item.toObject();
+      delete beat._id;
+      delete beat.video_id;
+      beats.push(beat);
+    });
+    res.json(beats);
   });
 };
 
@@ -53,16 +60,12 @@ module.exports.getAllPulses = (req, res, next) => {
     if (!data) {
       return res.status(404).send();
     }
-    res.json(parseBeats(data));
+    let beats = [];
+    data.forEach(item=> {
+      let beat = item.toObject();
+      delete beat._id;
+      beats.push(beat);
+    });
+    res.json(beats);
   });
 };
-
-function parseBeats(data) {
-  let beats = [];
-  data.forEach(item=> {
-    let beat = item.toObject();
-    delete beat._id;
-    beats.push(beat);
-  });
-  return beats;
-}
